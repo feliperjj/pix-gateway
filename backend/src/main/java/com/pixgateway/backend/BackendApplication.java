a76d14ea76deffce857320e869d681092a4d8589
Felipe Bento <81867174+feliperjj@users.noreply.github.com>
2026-04-07 14:22:09 +0000

    feat: add swagger ui for api documentation

--- backend/src/main/java/com/pixgateway/backend/BackendApplication.java
+++ backend/src/main/java/com/pixgateway/backend/BackendApplication.java
@@ -1,13 +1,16 @@
 package com.pixgateway.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Servidor Padrão") })
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
